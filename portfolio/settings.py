d0c585d07d9d4f89967c985f6b1cf533d523252c
Chris McClarin <eternalrook@gmail.com>
2023-06-13 02:19:19 -0700

    order change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #configure Djanog App for Heroku
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
